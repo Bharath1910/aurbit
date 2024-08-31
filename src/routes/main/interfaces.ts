74fad0fe1c0f641e33b21c0975233320f42190bf
Bharath1910 <av.bharath@proton.me>
2024-08-31 18:42:12 +0530

    updated sidebar to show logged in user

--- src/routes/main/interfaces.ts
+++ src/routes/main/interfaces.ts
@@ -1,17 +1,14 @@
 import { Request, Response, Router } from "express";
-import bcrypt from 'bcrypt';
-import { db } from "../../db/database";
-import { StatusCodes } from "http-status-codes";
-import * as jwt from "jsonwebtoken";
 import { authForgiving } from "../../middlewares/auth";
-import { STATUS_CODES } from "http";
 
 const interfaces = Router();
 
 interfaces.use(authForgiving);
 interfaces.get("/navbar", (req, res) => {
-	console.log(res.locals)
 	res.render("interfaces/navbar");
 })
 
+interfaces.get("/sidebar", (req, res) => {
+	res.render("interfaces/sidebar");
+})
 export default interfaces;
