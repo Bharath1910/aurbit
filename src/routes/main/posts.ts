1ac9403750b240629da4bcea2e10cceea1b03a5b
Bharath1910 <av.bharath@proton.me>
2024-12-12 21:34:45 +0530

    added uploading post functionality

--- src/routes/main/posts.ts
+++ src/routes/main/posts.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { authForgiving } from "../../middlewares/auth";
 import { db } from "../../db/database";
+import { StatusCodes } from "http-status-codes";
 
 const posts = Router();
 
@@ -19,5 +20,41 @@ posts.get("/", async(req, res) => {
 	res.locals.posts = posts;
 	res.render("home/post");
 })
+
+posts.post("/", async(req: Request, res: Response) => {
+	if (!req.body.community || !req.body.title || !req.body.content) {
+		res.header("HX-Retarget", "#error");
+		res.status(StatusCodes.BAD_REQUEST).send("the fields community, title and content are required");
+		return;
+	}
+
+	try {
+		await db
+			.insertInto('posts')
+			.values({
+				community_id: req.body.community,
+				title: req.body.title,
+				content: req.body.content,
+				username: res.locals.username,
+			})
+			.returning('id')
+			.execute();
+
+		res.header("HX-Redirect", "/");
+		res.status(StatusCodes.CREATED).send();
+		return;
+	} catch (err: any) {
+		if (err.code === '23503') {
+			res.header("HX-Retarget", "#error");
+			res.status(StatusCodes.NOT_FOUND).send("Community not found");
+			return;
+		}
+
+		console.log(err);
+		res.header("HX-Retarget", "#error");
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("An error occurred, please try again later");
+		return;
+	}
+});
 
 export default posts;
