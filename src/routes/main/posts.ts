5af2aeef49f9a824c11aec382dcdcdc6f1858bc5
Bharath1910 <av.bharath@proton.me>
2024-12-16 11:09:47 +0530

    added root comment handler

--- src/routes/main/posts.ts
+++ src/routes/main/posts.ts
@@ -38,6 +38,31 @@ posts.get("/:id/comments", async (req, res ) => {
 	res.render("home/comment");
 });
 
+posts.post("/:id/comments", async (req, res ) => {
+	if (!res.locals.authenticated) {
+		res.status(StatusCodes.UNAUTHORIZED).send("Unauthorized");
+		return;
+	}
+
+	if (!req.params.id || !req.body.content) {
+		res.status(StatusCodes.BAD_REQUEST).send("the field id is required");
+		return;
+	}
+
+	const comments = await db
+		.insertInto('comments')
+		.values({
+			author: res.locals.username,
+			content: req.body.content,
+			post_id: req.params.id
+		})
+		.returningAll()
+		.execute();
+
+	res.locals.comments = comments;
+	res.render("home/comment");
+});
+
 posts.post("/", async(req: Request, res: Response) => {
 	if (!req.body.community || !req.body.title || !req.body.content) {
 		res.header("HX-Retarget", "#error");
